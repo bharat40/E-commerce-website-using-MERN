943f743c49b74156a13eed58164d3de2ce7cac38
bharat <bharatdhiman40@gmail.com>
2024-09-05 09:36:48 +0530

    Inventory responsive

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -8,14 +8,18 @@ const ProductCard = ({ name, category, description, price, image }) => {
 
   return (
     <div
-      className={`border p-2 flex gap-3 w-[500px] bg-gray-900 hover:bg-gray-800 ${
+      className={`border flex flex-col  md:flex-row p-2  gap-3 w-[500px] bg-gray-900 hover:bg-gray-800 ${
         isClicked ? "border-green-500 border-4" : "border-gray-300"
       }`}
       onClick={handleClick}
     >
       <div className="w-[150px] h-[150px]">
         {/* product image */}
-        <img src={image} alt="product-image" className="w-full h-full object-cover" />
+        <img
+          src={image}
+          alt="product-image"
+          className="w-full h-full object-cover"
+        />
       </div>
       <div className="text-gray-200">
         {/* product details */}
