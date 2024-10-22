d76d6cbb0c72a52c913e2a69cb0c2072d01da8c9
bharat <bharatdhiman40@gmail.com>
2024-10-22 11:57:24 +0530

    moved product routes to routes folder and updated README file

---
+++ backend/routes/ProductRoutes.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const router = express.Router();
+const product = require('../models/Product');
+
+// router to get all products in database
+router.get('/', async (req, res) => {
+    try {
+        const data = await product.find();
+        res.status(200).json(data);
+    } catch (error) {
+        console.log("Error: ", error);
+        res.status(500).json({ error: "Internal server error" })
+    }
+})
+
+// router to add product in database
+router.post('/', async (req, res) => {
+    try {
+        const newProductData = req.body;
+
+        // Create a new instance of the Product model
+        const newProduct = new product(newProductData);
+
+        // Save the new product to the database
+        const savedProduct = await newProduct.save();
+
+        // Respond with the saved product and a 201 status code
+        res.status(200).json(savedProduct);
+    } catch (error) {
+        console.log("Error: ", error);
+        res.status(500).json({ Error: "Internal server error" })
+    }
+})
+
+
+// router to update product in database
+router.put('/:id', async (req, res) => {
+    try {
+        const productId = req.params.id;
+        const productData = req.body;
+        if (!mongoose.Types.ObjectId.isValid(productId)) {
+            return res.status(400).json({ Error: "Invalid product ID" });
+        }
+        const updatedProduct = await product.findByIdAndUpdate(productId, productData, {
+            new: true,
+            runValidators: true
+        })
+        if (!updatedProduct) {
+            res.status(404).json({ error: "Product not found" })
+        }
+        res.status(200).json(updatedProduct);
+    } catch (error) {
+        console.log("Error: ", error);
+        res.status(500).json({ Error: "Internal server error" })
+    }
+})
+
+// router to delete product in database
+router.delete('/:id', async (req, res) => {
+    try {
+        const productId = req.params.id;
+        if (!mongoose.Types.ObjectId.isValid(productId)) {
+            return res.status(400).json({ Error: "Invalid product ID" });
+        }
+        const deletedProduct = await product.findByIdAndDelete(productId);
+        if (!deletedProduct) {
+            res.status(404).json({ error: "Product not found" })
+        }
+        res.status(200).json({ message: "product deleted" })
+    } catch (error) {
+        console.log("Error: ", error);
+        res.status(500).json({ error: "Internal server error" })
+    }
+})
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -5,83 +5,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const cors = require('cors');
 const product = require('./models/Product');
+const productRoutes = require('./routes/ProductRoutes');
 // middleware
 app.use(cors());
 app.use(express.json());
 
+app.use('/product', productRoutes);
 
 
-// router to get all products in database
-app.get('/product', async (req, res) => {
-    try {
-        const data = await product.find();
-        res.status(200).json(data);
-    } catch (error) {
-        console.log("Error: ", error);
-        res.status(500).json({ error: "Internal server error" })
-    }
-})
-
-// router to add product in database
-app.post('/product', async (req, res) => {
-    try {
-        const newProductData = req.body;
-
-        // Create a new instance of the Product model
-        const newProduct = new product(newProductData);
-
-        // Save the new product to the database
-        const savedProduct = await newProduct.save();
-
-        // Respond with the saved product and a 201 status code
-        res.status(200).json(savedProduct);
-    } catch (error) {
-        console.log("Error: ", error);
-        res.status(500).json({ Error: "Internal server error" })
-    }
-})
-
-
-// router to update product in database
-app.put('/product/:id', async (req, res) => {
-    try {
-        const productId = req.params.id;
-        const productData = req.body;
-        if (!mongoose.Types.ObjectId.isValid(productId)) {
-            return res.status(400).json({ Error: "Invalid product ID" });
-        }
-        const updatedProduct = await product.findByIdAndUpdate(productId, productData, {
-            new: true,
-            runValidators: true
-        })
-        if (!updatedProduct) {
-            res.status(404).json({ error: "Product not found" })
-        }
-        res.status(200).json(updatedProduct);
-    } catch (error) {
-        console.log("Error: ", error);
-        res.status(500).json({ Error: "Internal server error" })
-    }
-})
-
-
-// router to delete product in database
-app.delete('/product/:id', async (req, res) => {
-    try {
-        const productId = req.params.id;
-        if (!mongoose.Types.ObjectId.isValid(productId)) {
-            return res.status(400).json({ Error: "Invalid product ID" });
-        }
-        const deletedProduct = await product.findByIdAndDelete(productId);
-        if (!deletedProduct) {
-            res.status(404).json({ error: "Product not found" })
-        }
-        res.status(200).json({ message: "product deleted" })
-    } catch (error) {
-        console.log("Error: ", error);
-        res.status(500).json({ error: "Internal server error" })
-    }
-})
 
 
 
